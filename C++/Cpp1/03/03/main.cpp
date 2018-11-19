8f4dc7fd9a52b60f25081b65655a65249c084433
swae <xiaoyuan1314@me.com>
2018-11-19 20:21:00 +0800

    Add 03

---
+++ C++/Cpp1/03/03/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  03
+//
+//  Created by swae on 2018/11/19.
+//  Copyright © 2018 xiaoyuan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
