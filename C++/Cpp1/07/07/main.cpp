73231a3006a04125ed44fbe8117fb2fa3b88ab44
xiaoyuan <seyooe@gmail.com>
2018-11-23 13:51:13 +0800

    Add 07

---
+++ C++/Cpp1/07/07/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  07
+//
+//  Created by xiaoyuan on 2018/11/23.
+//  Copyright © 2018 xiaoyuan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
